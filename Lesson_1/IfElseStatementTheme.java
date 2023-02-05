1b60d726ac363513eddd825ec8043782859edb12
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-02-05 15:40:24 +0300

    Исправление ошибок попытка 2

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -59,7 +59,7 @@ public class IfElseStatementTheme {
 
         System.out.println("\n4.Поиск одинаковых цифр в числах \n");
         num1 = 123;
-        num2 = 456;
+        num2 = 444;
         int num1Hundreds = num1 / 100;
         int num1Tens = num1 / 10 % 10;
         int num1Ones = num1 % 10;
@@ -67,6 +67,9 @@ public class IfElseStatementTheme {
         int num2Tens = num2 / 10 % 10;
         int num2Ones = num2 % 10;
         System.out.println("Исходные числа " + num1 + " и " + num2);
+        if (num1Hundreds != num2Hundreds && num1Tens != num2Tens && num1Ones != num2Ones) {
+            System.out.println("Все цифры в разрядах разные");
+        }
         if (num1Hundreds == num2Hundreds) {
             System.out.println("Одинаковые цифры: " + num1Hundreds + " разряд 1");
         }
@@ -75,9 +78,6 @@ public class IfElseStatementTheme {
         }
         if (num1Ones == num2Ones) {
             System.out.println("Одинаковые цифры: " + num1Ones + " разряд 3");
-        }
-        if (num1Hundreds != num2Hundreds && num1Tens != num2Tens && num1Ones != num2Ones) {
-            System.out.println("Все цифры в разрядах разные");
         }
 
         System.out.println("\n5.Определение символа по его коду \n");
@@ -94,11 +94,9 @@ public class IfElseStatementTheme {
 
         System.out.println("\n6.Подсчет суммы вклада и начисленных банком %\n");
         int depositAmount = 300000;
-        int percent = 0;
+        int percent = 7;
         if (depositAmount < 100000) {
             percent = 5;
-        } else if (depositAmount <= 300000 && depositAmount >= 100000) {
-            percent = 7;
         } else if (depositAmount > 300000) {
             percent = 10;
         }
@@ -109,100 +107,60 @@ public class IfElseStatementTheme {
         System.out.println("\n7.Определение оценки по предметам\n");
         int percentHistory = 59;
         int percentProgramming = 91;
-        int score2 = 2;
-        int score3 = 3;
-        int score4 = 4;
-        int score5 = 5;
-        if (percentHistory <= 60 && percentProgramming <= 60) {
-            System.out.println(score2 + " История\n" + score2 + " Программирование\nСредний балл: " 
-                    + "2\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory <= 60 && percentProgramming > 60 && percentProgramming <=73) {
-            System.out.println(score2 + " История\n" + score3 + " Программирование\nСредний балл: " 
-                    + "2.5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory <= 60 && percentProgramming > 73 && percentProgramming <=91) {
-            System.out.println(score2 + " История\n" + score4 + " Программирование\nСредний балл: " 
-                    + "3\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory <= 60 && percentProgramming > 91) {
-            System.out.println(score2 + " История\n" + score5 + " Программирование\nСредний балл: " 
-                    + "3.5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 60 && percentHistory <= 73 && percentProgramming <= 60) {
-            System.out.println(score3 + " История\n" + score2 + " Программирование\nСредний балл: " 
-                    + "2.5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 60 && percentHistory <= 73 && percentProgramming > 60 && 
-                percentProgramming <=73) {
-            System.out.println(score3 + " История\n" + score3 + " Программирование\nСредний балл: " 
-                    + "3\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 60 && percentHistory <= 73 && percentProgramming > 73 && 
-                percentProgramming <=91) {
-            System.out.println(score3 + " История\n" + score4 + " Программирование\nСредний балл: " 
-                    + "3.5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 60 && percentHistory <= 73 && percentProgramming > 91) {
-            System.out.println(score3 + " История\n" + score5 + " Программирование\nСредний балл: " 
-                    + "4\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 73 && percentHistory <= 91 && percentProgramming <= 60) {
-            System.out.println(score4 + " История\n" + score2 + " Программирование\nСредний балл: " 
-                    + "3\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 73 && percentHistory <= 91 && percentProgramming > 60 && 
-                percentProgramming <=73) {
-            System.out.println(score4 + " История\n" + score3 + " Программирование\nСредний балл: " 
-                    + "3.5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 73 && percentHistory <= 91 && percentProgramming > 73 && 
-                percentProgramming <=91) {
-            System.out.println(score4 + " История\n" + score4 + " Программирование\nСредний балл: " 
-                    + "4\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 73 && percentHistory <= 91 && percentProgramming > 91) {
-            System.out.println(score4 + " История\n" + score5 + " Программирование\nСредний балл: " 
-                    + "4.5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 91 && percentProgramming <= 60) {
-            System.out.println(score5 + " История\n" + score2 + " Программирование\nСредний балл: " 
-                    + "3.5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 91 && percentProgramming > 60 && 
-                percentProgramming <=73) {
-            System.out.println(score5 + " История\n" + score3 + " Программирование\nСредний балл: " 
-                    + "4\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 91 && percentProgramming > 73 && 
-                percentProgramming <=91) {
-            System.out.println(score5 + " История\n" + score4 + " Программирование\nСредний балл: " 
-                    + "4.5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
-        } else if (percentHistory > 91 && percentProgramming > 91) {
-            System.out.println(score5 + " История\n" + score5 + " Программирование\nСредний балл: " 
-                    + "5\nСредний %: " + ((percentHistory + percentProgramming) / 2));
+        int scoreHistory = 2;
+        int scoreProgramming = 2;
+        if (percentHistory > 60 && percentHistory <= 73) {
+            scoreHistory = 3;
+        } else if (percentHistory > 73 && percentHistory <= 91) {
+            scoreHistory = 4;
+        } else if (percentHistory > 91) {
+            scoreHistory = 5;
         }
-
+        if (percentProgramming > 60 && percentProgramming <= 73) {
+            scoreProgramming = 3;
+        } else if (percentProgramming > 73 && percentProgramming <= 91) {
+            scoreProgramming = 4;
+        } else if (percentProgramming > 91) {
+            scoreProgramming = 5;
+        }
+        System.out.println(scoreHistory + " История\n" + scoreProgramming + " Программирование\n" + 
+                ((scoreHistory + scoreProgramming) / 2) + " Средний балл оценок по предметам\n" + 
+                ((percentHistory + percentProgramming) / 2) + " Средний % оценок по предметам");
 
         System.out.println("\n8.Расчет прибыли за год\n");
         int rent = 5000;
         int revenue = 13000;
         int costPrice = 9000;
         int profitMonth = revenue - rent - costPrice;
-        if (profitMonth < 0) {
-            System.out.println("Прибыль за год: " + profitMonth * 3 + " руб.");
-        } else if (profitMonth > 0) {
+        if (profitMonth > 0) {
             System.out.println("Прибыль за год: +" + profitMonth * 12 + " руб.");
+        } else {
+            System.out.println("Прибыль за год: " + profitMonth * 12 + " руб.");
         }
 
         System.out.println("\n9.Подсчет количества банкнот\n");
         int sumNeeded = 567;
-        int banknotesDenomination1 = 50; // кол-во банкнот номиналом 1 в наличии
-        int banknotesDenomination10 = 5; // кол-во банкнот номиналом 10 в наличии
-        int banknotesDenomination100 = 10; // кол-во банкнот номиналом 100 в наличии
-        int neededBanknotesDenomination1 = sumNeeded % 10; // кол-во необходимых банкнот номиналом 1
-        int neededBanknotesDenomination10 = sumNeeded / 10 % 10; // кол-во необходимых банкнот номиналом 10
-        int neededBanknotesDenomination100 = sumNeeded / 100; // кол-во необходимых банкнот номиналом 100
-        if (neededBanknotesDenomination100 > banknotesDenomination100) {
-            neededBanknotesDenomination10 += (neededBanknotesDenomination100 -
-                banknotesDenomination100) * 10;
-        }
-        if (neededBanknotesDenomination10 > banknotesDenomination10) {
-            neededBanknotesDenomination1 += (neededBanknotesDenomination10 -
-                banknotesDenomination10) * 10;
-        }
-        if (neededBanknotesDenomination1 > banknotesDenomination1) {
+        int banknotesNominal1 = 50;
+        int banknotesNominal10 = 5;
+        int banknotesNominal100 = 10; 
+        int neededBanknotesNominal1 = sumNeeded % 10;
+        int neededBanknotesNominal10 = sumNeeded / 10 % 10;
+        int neededBanknotesNominal100 = sumNeeded / 100;
+        if (neededBanknotesNominal1 > banknotesNominal1) {
             System.out.println("Не хватает банкнот для выполнения операции");
-        } else if (neededBanknotesDenomination1 <= banknotesDenomination1) {
-            System.out.println("Номиналы банкнот\n100 - " + neededBanknotesDenomination100 + 
-                    "\n10 - " + neededBanknotesDenomination10 + "\n1 - " + 
-                    neededBanknotesDenomination1 + "\nСумма: " + sumNeeded);
+        }
+        if (neededBanknotesNominal100 > banknotesNominal100) {
+            neededBanknotesNominal10 += (neededBanknotesNominal100 -
+                banknotesNominal100) * 10;
+        }
+        if (neededBanknotesNominal10 > banknotesNominal10) {
+            neededBanknotesNominal1 += (neededBanknotesNominal10 -
+                banknotesNominal10) * 10;
+        }
+        if (neededBanknotesNominal1 <= banknotesNominal1) {
+            System.out.println("Номиналы банкнот\n100 - " + neededBanknotesNominal100 + 
+                    "\n10 - " + neededBanknotesNominal10 + "\n1 - " + 
+                    neededBanknotesNominal1 + "\nСумма: " + sumNeeded);
         }
     }
 }
