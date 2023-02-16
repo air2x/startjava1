9e321dfcab5f09747122a2122f2151be39ad97cf
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-02-16 20:01:17 +0300

    Исправил задания 2, 4, 6, 8, 9

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -22,19 +22,17 @@ public class CyclesTheme {
         int num1 = 10;
         int num2 = 5;
         int num3 = -1;
-        startRange = num3;
-        endRange = num1;
-        if (startRange > num1) {
-            startRange = num1;
-        } else if (startRange > num2) {
-            startRange = num2;
+        int min = num3;
+        int max = num1;
+        if (min > num1) {
+            min = num1;
+        } else if (min > num2) {
+            min = num2;
         }
-        if (endRange < num2) {
-            endRange = num2;
-        } else if (endRange < num3) {
-            startRange = num3;
+        if (max < num2) {
+            max = num2;
         }
-        for (int i = endRange - 1; i > startRange; i--) {
+        for (int i = max - 1; i > min; i--) {
             System.out.print(i + " ");
         }
 
@@ -55,11 +53,11 @@ public class CyclesTheme {
         num2 = 24;
         int count = 0;
         for (int i = num1; i < num2; i += 2) {
-            count++;
-            System.out.printf("%3d", i);
             if (count % 5 == 0) {
                 System.out.println();
             }
+            System.out.printf("%3d", i);
+            count++;
         }
         while (count % 5 != 0) {
             System.out.printf("%3d", 0);
@@ -90,32 +88,32 @@ public class CyclesTheme {
             System.out.println();
         }
 
-        int numString = 5;
-        while (numString > 0) {
-            int numColumn = numString;
-            while (numColumn > 0) {
+        int numLines = 5;
+        while (numLines > 0) {
+            int numColumns = numLines;
+            while (numColumns > 0) {
                 System.out.print("#");
-                numColumn--;
+                numColumns--;
             }
             System.out.println();
-            numString--;
+            numLines--;
         }
 
-        numString = 3;
-        int numColumn = 1 - numString;
+        int maxCharNum = 3;
+        int numColumns = 1 - maxCharNum;
         do {
-            int symbolWrite = 1;
-            int revers = numColumn;
+            int charNum = 1;
+            int revers = numColumns;
             if (revers < 0) {
                 revers = -revers;
             }
             do {
                 System.out.print("$");
-                ++symbolWrite;
-            } while (symbolWrite <= numString - revers);
+                ++charNum;
+            } while (charNum <= maxCharNum - revers);
             System.out.println();
-            ++numColumn;
-        } while (numColumn <= numString - 1);
+            ++numColumns;
+        } while (numColumns <= maxCharNum - 1);
 
         System.out.println("\n7. Отображение ASCII-символов");
         System.out.printf("%6s%6s%n", "Dec", "Char");
@@ -129,49 +127,45 @@ public class CyclesTheme {
 
         System.out.println("\n8. Проверка, является ли число палиндромом");
         num = 1234321;
-        int numRev = 0;
+        int revNum = 0;
         int numCopy = num;
         while (num > 0) {
-            numRev = numRev * 10 + (num % 10);
+            revNum = revNum * 10 + (num % 10);
             num /= 10;
         }
-        if (numCopy == numRev) {
+        if (numCopy == revNum) {
             System.out.println(numCopy + " является палиндромом");
         } else {
             System.out.println(numCopy + " не является палиндромом");
         }
 
         System.out.println("\n9. Определение, является ли число счастливым");
-        num = 441333;
-        numCopy = num;
-        numRev = 0;
-        int numDigits = 0;
-        int sumPartFirst = 0;
-        int sumPartSecond = 0;
-        while (num > 0) {
-            numRev = numRev * 10 + num %  10;
+        num = 411331;
+        int sum123 = 0;
+        int sum456 = 0;
+        int remains = 0;
+        int revSum123 = 0;
+        int revSum456 = 0;
+        revNum = 0;
+        for (int i = 0; i < 6; i++) {
+            remains = num % 10;
+            revNum = revNum * 10 + remains;
+            if (i < 3) {
+                sum123 += remains;
+                revSum123 = revSum123 * 10 + remains;
+            }
+            if (i >= 3) {
+                sum456 += remains;
+                revSum456 = revSum456 * 10 + remains;
+            }
             num /= 10;
-            numDigits++;
         }
-        System.out.print("Сумма цифр ");
-        for (int i = 0; i < numDigits / 2; i++) {
-            int numDigit = numCopy % 10;
-            sumPartFirst += numDigit;
-            System.out.print(numDigit);
-            numCopy /= 10;
-        }
-        System.out.print(" = " + sumPartFirst + "\nСумма цифр ");
-        for (int i = 0; i < numDigits / 2; i++) {
-            int numDigit = numRev % 10;
-            sumPartSecond += numDigit;
-            System.out.print(numDigit);
-            numRev /= 10;
-        }
-        System.out.print(" = " + sumPartSecond);
-        if (sumPartFirst == sumPartSecond) {
-            System.out.println("\nЧисло является счастливым");
+        System.out.println("Сумма цифр " + revSum123 + " = " + sum123 + "\nСумма цифр " +
+            revSum456 + " = " + sum456);
+        if (sum123 == sum456) {
+            System.out.println("Число счастливое");
         } else {
-            System.out.println("\nЧисло не является счастливым");
+            System.out.println("Число не счастливое");
         }
 
         System.out.println("\n10. Вывод таблицы умножения Пифагора");
