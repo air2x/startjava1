ebc9660eb6efc73deffa24de5278b8a6a11dc8cc
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-02-20 20:42:29 +0300

    Исправил названия

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -3,19 +3,17 @@ public class MyFirstGame {
     public static void main(String[] args) {
         int startRange = 0;
         int endRange = 100;
-        int numHidden = 50;
-        int numPlayer = 53;
-        while (numHidden != numPlayer) {
-            if (numHidden < numPlayer) {
-                System.out.println("Число " + numPlayer + " больше того, что загадал компьютер");
-                numPlayer--;
-            } else if (numHidden > numPlayer) {
-                System.out.println("Число " + numPlayer + " меньше того, что загадал компьютер");
-                numPlayer++;
+        int hiddenNum = 50;
+        int playerNum = 85;
+        while (hiddenNum != playerNum) {
+            if (hiddenNum < playerNum) {
+                System.out.println("Число " + playerNum + " больше того, что загадал компьютер");
+                playerNum--;
+            } else if (hiddenNum > playerNum) {
+                System.out.println("Число " + playerNum + " меньше того, что загадал компьютер");
+                playerNum++;
             }
         }
-        if (numHidden == numPlayer) {
-                System.out.println("Вы победили!");
-            }
+        System.out.println("Вы победили!");
     }
 }
