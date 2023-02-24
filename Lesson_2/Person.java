fea608218d7dc984a4de3e96edce88eabe23a1ca
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-02-24 17:57:32 +0300

    origin

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "Jon";
+    String gender = "man";
+    double height = 1.85;
+    double weight = 95.8;
+    int age = 28;
+
+    void walk () {
+        
+    }
+
+    void sit () {
+        
+    }
+
+    void run () {
+        
+    }
+
+    void talk () {
+        
+    }
+
+    void learnJava () {
+
+    }
+}
