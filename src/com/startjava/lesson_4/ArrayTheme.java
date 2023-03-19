8b11f143a32e04a97f9a2e3b6a2ad4b37b895fb0
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-03-19 17:15:02 +0300

    "Исправил задания 1-3, добавил методы"

--- src/com/startjava/lesson_4/ArrayTheme.java
+++ src/com/startjava/lesson_4/ArrayTheme.java
@@ -1,53 +1,56 @@
 package com.startjava.lesson_4;
+//import java.util.Arrays;
 public class ArrayTheme {
     public static void main(String[] args) {
         System.out.println("1. Реверс значений массива");
-        int[] intNums = {4, 2, 1, 7, 5, 3};
-        int[] intNumsRevers = new int[intNums.length];
-        int counter = 0;
-        for (int i = intNums.length - 1; i >= 0; i--) {
-            intNumsRevers[i] = intNums[counter++];
-        }
-        for (int intNum : intNums) {
-            System.out.print(intNum + " ");
+        int[] intNums = {4, 2, 1, 7, 5, 3, 6};
+        int len = intNums.length;
+        ArrayTheme.printIntNums(intNums);
+        int temp;
+        for (int i = 0; i < len / 2; i++) {
+            temp = intNums[len - i - 1];
+            intNums[len - i - 1] = intNums[i];
+            intNums[i] = temp;
         }
         System.out.println();
-        for (int intNumRevers : intNumsRevers) {
-            System.out.print(intNumRevers + " ");
-        }
+        ArrayTheme.printIntNums(intNums);
 
         System.out.println("\n\n2. Вывод произведения элементов массива");
-        intNums = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        intNums = new int[10];
+        len = intNums.length;
+        int productNum = 1;
+        for (int i = 0; i < len; i++) {
+            intNums[i] = i;
+        }
+        for (int i = 1; i < len - 1; i++) {
+            productNum *= intNums[i];
+        }
         for (int intNum : intNums) {
             if (intNum == 0 || intNum == 9) {
                 continue;
             }
-            System.out.print(intNum + (intNum == 8 ? " " : " * "));
+            System.out.print(intNum + (intNum == intNums[len - 2] ? " " : " * "));
         }
-        System.out.println("= R\n" + intNums[0] + " " + intNums[9]);
+        System.out.println("= " + productNum + "\n" + intNums[0] + " " + intNums[9]);
 
         System.out.println("\n3. Удаление элементов массива");
-        double[] dubArr = {0.64, 0.3, 0.54, 0.35, 0.83, 0.41, 0.46, 0.58, 0.57, 0.6, 0.63, 0, 0.69, 0.84, 0.38};
+        double[] dubArr = new double[15];
+        for (int i = 0; i < dubArr.length; i++) {
+            dubArr[i] = Math.random();
+        }
         int midCell = dubArr.length / 2;
         System.out.println("Исходный массив:");
-        for (int i = 0; i < dubArr.length; i++) {
-            if (i == 8) {
-                System.out.println();
-            }
-            System.out.printf("%7.3f", dubArr[i]);
-        }
+        ArrayTheme.printDubArr(dubArr);
         System.out.println("\nИзмененный массив:");
+        int count = 0;
         for (int i = 0; i < dubArr.length; i++) {
             if (dubArr[i] > dubArr[midCell]) {
                 dubArr[i] = 0;
+                count++;
             }
         }
-        for (int i = 0; i < dubArr.length; i++) {
-            if (i == 8) {
-                System.out.println();
-            }
-            System.out.printf("%7.3f", dubArr[i]);
-        }
+        ArrayTheme.printDubArr(dubArr);
+        System.out.println("\nКоличество обнуленных ячеек: " + count);
 
         System.out.println("\n\n4. Вывод элементов массива лесенкой в обратном порядке");
         char[] charArr = new char[27];
@@ -60,6 +63,37 @@ public class ArrayTheme {
                 System.out.print(charArr[j]);
             }
             System.out.println();
+        }
+
+//        System.out.println("\n5. Генерация уникальных чисел");
+//        int[] intArr = new int[30];
+//        for (int i = 0; i < intArr.length; i++) {
+//            intArr[i] = 60 + (int) (Math.random() * 40);
+//        }
+//        count = 0;
+//        Arrays.sort(intArr);
+//        for (int intNumArr : intArr) {
+//            if (count % 10 == 0) {
+//                System.out.println();
+//            }
+//            System.out.print(intNumArr + " ");
+//            count++;
+//        }
+
+//        System.out.println("\n6. Копирование не пустых строк");
+//        String[] strArr = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
+    }
+    private static void printIntNums(int[] intNums) {
+        for (int intNum : intNums) {
+            System.out.print(intNum + " ");
+        }
+    }
+    private static void printDubArr(double[] dubArr) {
+        for (int i = 0; i < dubArr.length; i++) {
+            if (i == 8) {
+                System.out.println();
+            }
+            System.out.printf("%7.3f", dubArr[i]);
         }
     }
 }
