b26ec01bfd1c17baf69170db48475255f7801247
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-04-02 19:11:36 +0300

    "Упростил return switch (elements[1]), цикл проверяет ответ пользователя"

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -6,8 +6,7 @@ public class Calculator {
         String[] elements = mathExpression.split(" ");
         int num1 = Integer.parseInt(elements[0]);
         int num2 = Integer.parseInt(elements[2]);
-        String mathSign = elements[1];
-        return switch (mathSign) {
+        return switch (elements[1]) {
             case "+" -> num1 + num2;
             case "-" -> num1 - num2;
             case "*" -> num1 * num2;
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -7,20 +7,15 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String answer = "yes";
-        String question = "Хотите продолжить вычисления? [yes/no]";
-        while (true){
-            if (!answer.equals("no") && !answer.equals("yes")) {
-                System.out.println(question);
-                answer = scanner.nextLine();
-            } else if (answer.equals("yes")) {
+        do {
+            if (answer.equals("yes")) {
                 System.out.println("Введите математическое выражение: ");
                 String mathExpression = scanner.nextLine();
                 double result = Calculator.calculate(mathExpression);
                 System.out.printf(((result == 0) ? "%.0f\n" : "%.3f\n"), result);
-                answer = "answer";
-            } else {
-                break;
             }
-        }
+            System.out.println("Хотите продолжить вычисления? [yes/no]");
+            answer = scanner.nextLine();
+        } while (!answer.equals("no"));
     }
 }
